94e335c2dbd3f768e5d1f3dd5a332bbacecc56cd
Lazar <lazazr@gmail.com>
2019-07-18 00:11:06 +0200

    Get request on OpenWeather Api with Axios

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -9,26 +9,36 @@
             <input v-model="cityName" 
               class="bg-white outline-none focus:border-teal-500 border-2 border-gray-300 rounded-l-lg py-2 px-4 block w-full appearance-none leading-normal text-lg text-teal-800 font-bold" 
               type="text" placeholder="Tapez le nom de votre ville...">
-            <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 border-2 text-white py-2 px-4 rounded-r-lg text-lg appearance-none outline-none" type="button">
+            <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 border-2 text-white py-2 px-4 rounded-r-lg text-lg appearance-none outline-none" 
+                    type="button" @click="onSubmit">
               <i class="fas fa-search"></i>
             </button>
           </div>
         </form>
-        <h1>{{cityName}}</h1>
     </div>
   </div>
   <div v-else class="h-full bg-gray-600">
-    
+
   </div>
 </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       hasCityName: false,
       cityName: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.cityName)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&APPID=5930ff12f2a177a4379b45eb1959f053`)
+        .then(res => console.log(res))
+        .catch(error => console.log(error));
     }
   },
   mounted() {
