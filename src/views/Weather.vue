809b8a62a774884fe50c728e0692d56264b425e5
Lazar <lazazr@gmail.com>
2019-07-19 21:13:57 +0200

    Weather: Error handling for the name input

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -4,9 +4,9 @@
     <div>
       <img alt="Vue logo" src="../assets/weather-anim.gif" class="block m-auto h-64 w-auto">
       <h1 class="text-5xl font-black text-teal-700 uppercase">Vue-Weather</h1>
-        <form class="w-full max-w-sm">
-          <div class="flex items-center">
-            <input v-model="cityName" 
+        <form @submit.prevent class="relative w-full max-w-sm">
+          <div class=" flex items-center">
+            <input v-model="cityName" @keydown.enter.prevent @keyup.enter="onSubmit"
               class="bg-white outline-none focus:border-teal-500 border-2 border-gray-300 rounded-l-lg py-2 px-4 block w-full appearance-none leading-normal text-lg text-teal-800 font-bold" 
               type="text" placeholder="Tapez le nom de votre ville...">
             <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 border-2 text-white py-2 px-4 rounded-r-lg text-lg appearance-none outline-none" 
@@ -15,6 +15,18 @@
             </button>
           </div>
         </form>
+        <div v-if="hasError" class="absolute mt-2 error-message text-sm ml-4">
+          <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded relative flex" role="alert">
+            <strong class="font-bold mr-2">Aïe ! </strong>
+            <span class="block sm:inline mr-2">La ville recherchée est introuvable...</span>
+              <svg
+                @click="hasError = false"
+                class="inline fill-current h-6 w-6 text-red-500" 
+                role="button" xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 20 20"><title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+          </div>
+        </div> 
     </div>
   </div>
   <div v-else class="h-full bg-gray-600">
@@ -30,19 +42,24 @@ export default {
   data() {
     return {
       hasCityName: false,
-      cityName: ''
+      cityName: '',
+      hasError: false
     }
   },
   methods: {
     onSubmit () {
       console.log(this.cityName)
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&APPID=5930ff12f2a177a4379b45eb1959f053`)
-        .then(res => console.log(res))
-        .catch(error => console.log(error));
+        .then(res => {
+          console.log(res)
+          this.$store.state.minifyNavbar = true; 
+          this.hasCityName = true;
+        })
+        .catch(error => {
+          console.log(error);
+          this.hasError = true;
+        });
     }
-  },
-  mounted() {
-    this.$store.state.minifyNavbar = true; 
   },
   destroyed() {
     this.$store.state.minifyNavbar = false; 
