9f1b82f931b822e65abfdf2677b669e0b16aba8e
Lazar <lazazr@gmail.com>
2019-08-31 12:40:55 +0200

    Pokemon: Fetch and store pokemon list in LocalStorage

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>Coming soon!</h1>
+  <div class="h-screen">
   </div>
 </template>
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      pokemonList: [],
+      sliderMaxValue: Number
+    }
+  },
+  methods: {
+    fetchPokemonList() {
+      axios.get('https://pokeapi.co/api/v2/pokemon/?limit=1000')
+        .then(res => {
+          console.log(res);
+          this.sliderMaxValue = res.data.count - 1;
+          this.pokemonList = res.data.results;
+          localStorage.setItem('pokemonList', JSON.stringify(res.data.results));
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('pokemonList')) {
+      try {
+        this.pokemonList = JSON.parse(localStorage.getItem('pokemonList'));
+        this.sliderMaxValue = this.pokemonList.length - 1;
+      } catch (error) {
+        console.log('Error when retrieving data from local storage.')
+        localStorage.removeItem('pokemonList');
+        this.fetchPokemonList();
+      }
+    } else {
+      this.fetchPokemonList();
+    }
+  },
+
+
+}
+</script>
