1fe5109fea5c451a96612557bf30d08e2309fa87
Lazar <lazazr@gmail.com>
2019-08-31 21:39:39 +0200

    Pokemon: responsive tweaks

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,46 +1,46 @@
 <template>
-  <div v-if="this.pokemonList" class="h-screen flex flex-col justify-center items-center">
+  <div v-if="this.pokemonList" class="mt-10 h-screen flex flex-col justify-center items-center">
     <svg 
         class="h-16 w-auto inline-block fill-current text-red-800 " 
         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
         <path d="M500 10.1c-270.5 0-490 219.4-490 489.8 0 270.5 219.5 490 490 490s490-219.5 490-490S770.5 10.1 500 10.1zm0 343.2c81 0 146.8 65.6 146.8 146.6S581 646.7 500 646.7c-81 0-146.8-65.8-146.8-146.8 0-81 65.8-146.6 146.8-146.6zM84.7 531.4H290c15.2 102.3 103.5 180.7 210 180.7s194.8-78.4 210-180.7h205.3c-16.2 215.2-196 385.1-415.3 385.1S100.8 746.7 84.7 531.4z"/>
     </svg>
     <h1 class="text-4xl sm:text-5xl font-black text-red-700 uppercase">Vue-Pokédex</h1>
-    <form @submit.prevent class="relative w-full max-w-sm mb-10">
+    <form @submit.prevent class="relative w-full max-w-xs sm:max-w-sm sm:mb-10">
       <div class=" flex items-center">
         <input v-model="pokemonName" @keydown.enter.prevent @keyup.enter="onSubmit"
-          class="bg-white outline-none focus:border-red-500 border-2 border-gray-300 rounded-l-lg py-2 px-4 block w-full appearance-none leading-normal text-base sm:text-lg text-red-800 font-bold" 
+          class="bg-white outline-none focus:border-red-500 border-2 border-gray-300 rounded-l-lg py-2 px-4 block w-full appearance-none leading-normal text-sm sm:text-base sm:text-lg text-red-800 font-bold" 
           type="text" placeholder="Search a Pokémon by name or ID">
-        <button class="flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 border-2 text-white py-2 px-4 rounded-r-lg text-base sm:text-lg appearance-none outline-none" 
+        <button class="flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 border-2 text-white py-2 px-4 rounded-r-lg text-sm sm:text-base sm:text-lg appearance-none outline-none" 
                 type="button" @click="onSubmit">
           <i class="fas fa-search"></i>
         </button>
       </div>
     </form>
-    <div class="w-full h-72 flex items-center justify-center">
-      <div class="mr-6">
+    <div class="w-full h-56 sm:h-72 flex items-center justify-center">
+      <div class="mr-4 sm:mr-6">
         <span class="block italic text-red-500 font-black text-base text-center">#{{computedFirstId}}</span>
-        <h2 class="text-gray-800 font-black text-base text-center">{{pokemonList[computedFirstId - 1].name.toUpperCase()}}</h2>
-        <div :class="[computedFirstId === sliderCurrentValue ? 'border-2 border-red-400 bg-red-300 ' : 'bg-gray-200' , 'w-56 h-56 shadow-lg hover:shadow-2xl rounded-full flex-shrink-0 cursor-pointer flex items-center']">
+        <h2 class="text-gray-800 font-black text-sm sm:text-base text-center">{{pokemonList[computedFirstId - 1].name.toUpperCase()}}</h2>
+        <div :class="[computedFirstId === sliderCurrentValue ? 'border-2 border-red-400 bg-red-300 ' : 'bg-gray-200' , 'w-24 h-24 sm:w-56 sm:h-56 shadow-lg hover:shadow-2xl rounded-full flex-shrink-0 cursor-pointer flex items-center']">
           <img class="w-48 h-auto mx-auto" :src="getPokeImg(computedFirstId)" alt="pokemon">
         </div>
       </div>
-      <div class="mr-6">
+      <div class="mr-4 sm:mr-6">
         <span class="block italic text-red-500 font-black text-base text-center">#{{computedSecondtId}}</span>
-        <h2 class="text-gray-800 font-black text-base text-center">{{pokemonList[computedSecondtId - 1].name.toUpperCase()}}</h2>
-        <div :class="[computedSecondtId === sliderCurrentValue ? 'border-2 border-red-400 bg-red-300 ' : 'bg-gray-200' , 'w-56 h-56 shadow-lg hover:shadow-2xl rounded-full flex-shrink-0 cursor-pointer flex items-center']">
+        <h2 class="text-gray-800 font-black text-sm sm:text-base text-center">{{pokemonList[computedSecondtId - 1].name.toUpperCase()}}</h2>
+        <div :class="[computedSecondtId === sliderCurrentValue ? 'border-2 border-red-400 bg-red-300 ' : 'bg-gray-200' , 'w-24 h-24 sm:w-56 shadow-lg hover:shadow-2xl rounded-full flex-shrink-0 cursor-pointer flex items-center']">
           <img class="w-48 h-auto mx-auto" :src="getPokeImg(computedSecondtId)" alt="pokemon">
         </div>
       </div>
       <div>
         <span class="block italic text-red-500 font-black text-base text-center">#{{computedThirdtId}}</span>
-        <h2 class="text-gray-800 font-black text-base text-center">{{pokemonList[computedThirdtId - 1].name.toUpperCase()}}</h2>
-        <div :class="[computedThirdtId === sliderCurrentValue ? 'border-2 border-red-400 bg-red-300 ' : 'bg-gray-200' , 'w-56 h-56 shadow-lg hover:shadow-2xl rounded-full flex-shrink-0 cursor-pointer flex items-center']">
+        <h2 class="text-gray-800 font-black text-sm sm:text-base text-center">{{pokemonList[computedThirdtId - 1].name.toUpperCase()}}</h2>
+        <div :class="[computedThirdtId === sliderCurrentValue ? 'border-2 border-red-400 bg-red-300 ' : 'bg-gray-200' , 'w-24 h-24 sm:w-56 shadow-lg hover:shadow-2xl rounded-full flex-shrink-0 cursor-pointer flex items-center']">
           <img class="w-48 h-auto mx-auto" :src="getPokeImg(computedThirdtId)" alt="pokemon">
         </div>
       </div>
     </div>
-    <div class="w-82 h-32 mt-10 mb-20">
+    <div class="w-72 sm:w-82 h-32 mt-10 mb-20">
       <vue-slider
         v-model="sliderCurrentValue"
         :lazy="true"
